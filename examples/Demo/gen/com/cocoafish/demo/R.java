090acb511ab1ca95de4f5e541420bfda20d8ce0a
Bill Wang <zhengtao.wang@yahoo.com>
2011-09-12 16:59:48 +0800

    Remove references

--- examples/Demo/gen/com/cocoafish/demo/R.java
+++ examples/Demo/gen/com/cocoafish/demo/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f060001;
-        public static final int white=0x7f060000;
+        public static final int black=0x7f050001;
+        public static final int white=0x7f050000;
     }
     public static final class drawable {
         public static final int bubble=0x7f020000;
@@ -72,21 +72,20 @@ public final class R {
         public static final int userview=0x7f030008;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int cancel=0x7f050009;
-        public static final int checkin=0x7f05000b;
-        public static final int details=0x7f05000e;
-        public static final int done=0x7f05000a;
-        public static final int email_address=0x7f050004;
-        public static final int first_name=0x7f050002;
-        public static final int hello=0x7f050000;
-        public static final int last_name=0x7f050003;
-        public static final int logout=0x7f05000d;
-        public static final int password=0x7f050005;
-        public static final int password_confirm=0x7f050006;
-        public static final int refresh=0x7f05000c;
-        public static final int signin=0x7f050008;
-        public static final int signup=0x7f050007;
+        public static final int app_name=0x7f060000;
+        public static final int cancel=0x7f060008;
+        public static final int checkin=0x7f06000a;
+        public static final int details=0x7f06000d;
+        public static final int done=0x7f060009;
+        public static final int email_address=0x7f060003;
+        public static final int first_name=0x7f060001;
+        public static final int last_name=0x7f060002;
+        public static final int logout=0x7f06000c;
+        public static final int password=0x7f060004;
+        public static final int password_confirm=0x7f060005;
+        public static final int refresh=0x7f06000b;
+        public static final int signin=0x7f060007;
+        public static final int signup=0x7f060006;
     }
     public static final class xml {
         public static final int default_values=0x7f040000;
