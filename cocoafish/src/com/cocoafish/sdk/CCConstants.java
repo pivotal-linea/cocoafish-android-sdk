c21fea695bef0917aaf6d04014abf0b3a15be1b0
Bill Wang <zhengtao.wang@yahoo.com>
2011-09-13 16:01:37 +0800

    Update date format

--- cocoafish/src/com/cocoafish/sdk/CCConstants.java
+++ cocoafish/src/com/cocoafish/sdk/CCConstants.java
@@ -27,5 +27,5 @@ public class CCConstants {
 	public static final String ID = "id";
 	public static final String UPDATED_AT = "updated_at";
 	public static final String CREATED_AT = "created_at";
-	public static final String DATE_FORMAT = "yyyyMMdd'T'HH:mm:ssZ";
+	public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
 }
